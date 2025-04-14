80dacfb0c44c9a9376f725b0ba60ca33c370733e
DosHuff <laura.reyes.esad@gmail.com>
2025-04-13 20:49:47 -0400

    mensaje

---
+++ groceryTracker/grocery_Tracker.js
@@ -0,0 +1,14 @@
+let grocery1;
+let grocery2;
+let grocery3;
+let grocery4;
+
+function total() {
+ grocery1 = parseFloat(document.getElementById('grocery1').value);
+ grocery2 = parseFloat(document.getElementById('grocery2').value);
+ grocery3 = parseFloat(document.getElementById('grocery3').value);
+ grocery4 = parseFloat(document.getElementById('grocery4').value);
+
+ let TOTAL = grocery1 + grocery2 + grocery3 + grocery4;
+  document.getElementById('TOTAL').innerText = `La cuenta total es: ${TOTAL}`;
+}
